967db267f255536bfbddf1d6c87881406668a4dc
Michael Poulshock <michael.poulshock@gmail.com>
2012-08-09 22:35:19 -0400

    Fixed line formatting

--- HammurabiInteractive/Interview/Interview - main.cs
+++ HammurabiInteractive/Interview/Interview - main.cs
@@ -22,55 +22,55 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using Hammurabi;
-	
+    
 namespace Interactive
 {
-	/// <summary>
-	/// Generates a DOS-based, utterly primitive interview designed
-	/// to illustrate how Hammurabi can simulate backwards chaining.
-	/// </summary>
-	public static partial class Interview 
-	{
-		// Create some entities that will be the subject of the interview.
-		// Later, this will have to be made more dynamic...
-		public static Thing p = new Thing("p");
-		public static Thing e = new Thing("the person"); 
-		public static Thing c   = new Thing("the employer");
-		public static Thing p1 = new Thing("the seller"); 
-		public static Thing p2 = new Thing("the buyer"); 
-		public static Thing prop = new Thing("the item"); 
+    /// <summary>
+    /// Generates a DOS-based, utterly primitive interview designed
+    /// to illustrate how Hammurabi can simulate backwards chaining.
+    /// </summary>
+    public static partial class Interview 
+    {
+        // Create some entities that will be the subject of the interview.
+        // Later, this will have to be made more dynamic...
+        public static Thing p = new Thing("p");
+        public static Thing e = new Thing("the person"); 
+        public static Thing c = new Thing("the employer");
+        public static Thing p1 = new Thing("the seller"); 
+        public static Thing p2 = new Thing("the buyer"); 
+        public static Thing prop = new Thing("the item"); 
 
-		/// <summary>
-		/// Assesses the state of asserted facts and then decides what to do next.
-		/// </summary>
-		public static void ProcessRequest() 
-		{
-			// Clear fact base and assert any assumed facts
-			Facts.Clear();
-            AssertPreliminaryFacts();	
+        /// <summary>
+        /// Assesses the state of asserted facts and then decides what to do next.
+        /// </summary>
+        public static void ProcessRequest() 
+        {
+            // Clear fact base and assert any assumed facts
+            Facts.Clear();
+            AssertPreliminaryFacts();    
 
-			// Initialize the .akk unit test
-			AkkTest.testStr = "";
-			AkkTest.InitializeUnitTest();
+            // Initialize the .akk unit test
+            AkkTest.testStr = "";
+            AkkTest.InitializeUnitTest();
 
-			// Load the goals to be investigated (onto the goals list)
-			List<Func<Tvar>> goals = new List<Func<Tvar>>();
+            // Load the goals to be investigated (onto the goals list)
+            List<Func<Tvar>> goals = new List<Func<Tvar>>();
 
-//			goals.Add(()=> USC.Tit8.Sec1403.IsUSCitizenPerCanalZone(e));
-			goals.Add(()=> USC.Tit8.Sec1404.IsUSCitizenPerAlaska(p));
+//            goals.Add(()=> USC.Tit8.Sec1403.IsUSCitizenPerCanalZone(e));
+            goals.Add(()=> USC.Tit8.Sec1404.IsUSCitizenPerAlaska(p));
 
-//			goals.Add(()=> Hammurabi.Sandbox.IsCitizen(e));
-//			goals.Add(()=> Facts.QueryTvar<Tbool>("IsAlive", e));
-//			goals.Add(()=> Facts.QueryTvar<Tdate>("DateOfBirth", e));
-//			goals.Add(()=> USC.Tit29.Sec2612.IsEntitledToLeaveFrom(e,c));
+//            goals.Add(()=> Hammurabi.Sandbox.IsCitizen(e));
+//            goals.Add(()=> Facts.QueryTvar<Tbool>("IsAlive", e));
+//            goals.Add(()=> Facts.QueryTvar<Tdate>("DateOfBirth", e));
+//            goals.Add(()=> USC.Tit29.Sec2612.IsEntitledToLeaveFrom(e,c));
 
-			while (true)
-			{
-				// Get the response object from the interview engine
-				Engine.Response response = Engine.Investigate(goals);
+            while (true)
+            {
+                // Get the response object from the interview engine
+                Engine.Response response = Engine.Investigate(goals);
 
-				// Ask the current question, or display the results
-				if (!response.InvestigationComplete)
+                // Ask the current question, or display the results
+                if (!response.InvestigationComplete)
                 {
                     // Ask the next question
                     DisplayQuestion(response); 
@@ -85,73 +85,73 @@ namespace Interactive
                 }
             }
 
-			// Display all facts that have been asserted (for diagnostic purposes)
-			// Console.WriteLine("\nFacts: \n" + Facts.AssertedFacts());
-		}
+            // Display all facts that have been asserted (for diagnostic purposes)
+            // Console.WriteLine("\nFacts: \n" + Facts.AssertedFacts());
+        }
 
-		/// <summary>
-		/// Gets the and validates the user's answer to a question.
-		/// </summary>
-		private static void GetAndParseAnswer(Engine.Response response)
-		{
-			// Get data pertaining to the current question
-			string currentRel = response.NextFact.relationship;
-			Question currentQuestion = Templates.GetQ(currentRel);
-			string currentQType = currentQuestion.questionType;
+        /// <summary>
+        /// Gets the and validates the user's answer to a question.
+        /// </summary>
+        private static void GetAndParseAnswer(Engine.Response response)
+        {
+            // Get data pertaining to the current question
+            string currentRel = response.NextFact.relationship;
+            Question currentQuestion = Templates.GetQ(currentRel);
+            string currentQType = currentQuestion.questionType;
 
-			// Read (and gently massage) the answer
-			string answer = Console.ReadLine();
-			answer = CleanBooleans(currentQType, answer);
+            // Read (and gently massage) the answer
+            string answer = Console.ReadLine();
+            answer = CleanBooleans(currentQType, answer);
 
-			// Validate answer, then assert it
-			if (AnswerIsValid(currentQuestion, answer))
-			{
-				AssertAnswer(response, answer);
-			}
-			else
-			{
-				GetAndParseAnswer(response);
-			}
-		}
+            // Validate answer, then assert it
+            if (AnswerIsValid(currentQuestion, answer))
+            {
+                AssertAnswer(response, answer);
+            }
+            else
+            {
+                GetAndParseAnswer(response);
+            }
+        }
 
-		/// <summary>
-		/// Asserts facts (for testing or pre-seeding purposes).
-		/// </summary>
-		private static void AssertPreliminaryFacts()   
-		{
-			// Put any facts to be asserted here, like this:
-			// Facts.Assert(e, r.HoursWorkedPerWeek, 44);
-		}
-		
-		/// <summary>
-		/// Displays a question to the user.
-		/// </summary>
-		private static void DisplayQuestion(Engine.Response response)  
-		{
-			// Get data about the next question
-			Facts.Factlet theFact = response.NextFact;
-			Question theQ = Templates.GetQ(theFact.relationship);
+        /// <summary>
+        /// Asserts facts (for testing or pre-seeding purposes).
+        /// </summary>
+        private static void AssertPreliminaryFacts()   
+        {
+            // Put any facts to be asserted here, like this:
+            // Facts.Assert(e, r.HoursWorkedPerWeek, 44);
+        }
+        
+        /// <summary>
+        /// Displays a question to the user.
+        /// </summary>
+        private static void DisplayQuestion(Engine.Response response)  
+        {
+            // Get data about the next question
+            Facts.Factlet theFact = response.NextFact;
+            Question theQ = Templates.GetQ(theFact.relationship);
 
-			// TODO: Display appropriate question control (using theQ.questionType)
+            // TODO: Display appropriate question control (using theQ.questionType)
 
-			// White space
-			Console.WriteLine();
+            // White space
+            Console.WriteLine();
 
-			// Display progress percentage
-			Console.WriteLine("Percent complete: " + response.PercentComplete);
+            // Display progress percentage
+            Console.WriteLine("Percent complete: " + response.PercentComplete);
 
-			// Display question text
-			string qText = QuestionText(theFact, theQ);
-			Console.WriteLine(qText);
-			AkkTest.AddUnitTestAssertRel(theFact, theQ);
+            // Display question text
+            string qText = QuestionText(theFact, theQ);
+            Console.WriteLine(qText);
+            AkkTest.AddUnitTestAssertRel(theFact, theQ);
 
-			// Display an explanation (if any) 
-			if (theQ.explanation != "")
-			{
-				Console.WriteLine("Note: " + theQ.explanation);
-			}
-		}
-		
+            // Display an explanation (if any) 
+            if (theQ.explanation != "")
+            {
+                Console.WriteLine("Note: " + theQ.explanation);
+            }
+        }
+        
         /// <summary>
         /// Returns the text of the question that should be displayed.
         /// </summary>
@@ -178,7 +178,7 @@ namespace Interactive
             Console.WriteLine(ResultsText(goals));
         }
 
-		/// <summary>
+        /// <summary>
         /// Displays the results of each goal.
         /// </summary>
         private static string ResultsText(List<Func<Tvar>> goals)
@@ -198,21 +198,21 @@ namespace Interactive
             return result;
         }
 
-		/// <summary>
-		/// Converts free-text DOS answers into valid booleans, if applicable.
-		/// </summary>
-		private static string CleanBooleans(string currentQType, string input)
-		{
-			string i = input.Trim();
+        /// <summary>
+        /// Converts free-text DOS answers into valid booleans, if applicable.
+        /// </summary>
+        private static string CleanBooleans(string currentQType, string input)
+        {
+            string i = input.Trim();
 
-			if (currentQType == "bool")
-			{
-				i = i.ToLower();
-				if (i == "t" || i == "yes" || i == "y") return "true";
-				if (i == "f" || i == "no" || i == "n") return "false";
-			}
+            if (currentQType == "bool")
+            {
+                i = i.ToLower();
+                if (i == "t" || i == "yes" || i == "y") return "true";
+                if (i == "f" || i == "no" || i == "n") return "false";
+            }
 
-			return i;
-		}
-	}
+            return i;
+        }
+    }
 }
